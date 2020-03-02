57f5ddc2211e407a3f16195ed7e6d115a122a045
snasphysicist <ih8coldplay@hotmail.com>
2020-03-02 20:48:17 +0800

    Correct template name for new duel view

--- run_duel/views.py
+++ run_duel/views.py
@@ -1,8 +1,9 @@
 from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
 def new_duel(request):
-    template = loader.get_template('run_duel/new_duel.html')
+    template = loader.get_template('run_duel/new.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
