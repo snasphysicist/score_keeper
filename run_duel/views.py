8908e2ec2caa67d29fb5591f335ba2f1d64c0826
snasphysicist <ih8coldplay@hotmail.com>
2020-03-02 20:24:20 +0800

    View for new duel page

--- run_duel/views.py
+++ run_duel/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+def new_duel(request):
+    template = loader.get_template('run_duel/new_duel.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
