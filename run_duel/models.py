43169cd54e74ddb81afb8bf8608c0a6ca2c0033f
snasphysicist <ih8coldplay@hotmail.com>
2020-03-02 20:15:34 +0800

    Data models for a new duel

--- run_duel/models.py
+++ run_duel/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Duel object
+class Duel(models.Model):
+    opponent1 = models.CharField(max_length=100)
+    opponent2 = models.CharField(max_length=100)
+    sequence_number = models.IntegerField(default=1)
+
+# Round object
+class Round(models.Model):
+    round_number = models.IntegerField(default=1)
+
+# Event object
+class FightEvent(models.Model):
+    time = models.DateTimeField('Occurred At')
+    type = models.CharField(max_length=20)
+
