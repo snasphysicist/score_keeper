d506435a4c2efd921b024eb4ff627220ddd1c84b
snasphysicist <ih8coldplay@hotmail.com>
2020-03-02 21:27:46 +0800

    Url for new duel api

--- run_duel/urls.py
+++ run_duel/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new', views.new_duel, name='new_duel')
+    path('new', views.new_duel, name='new_duel'),
+    path('api/v1/new_duel', views.new_duel_api, name='new_duel_api')
 ]
