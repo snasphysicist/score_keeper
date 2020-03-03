aa6db9917c6ad34c7b1fdf9ef041f7c3d7dc1bb1
snasphysicist <ih8coldplay@hotmail.com>
2020-03-04 01:19:54 +0800

    Helper functions to calculate time elapsed and round statuses

--- run_duel/views.py
+++ run_duel/views.py
@@ -2,6 +2,7 @@
 import datetime
 import json
 
+from django.db import IntegrityError
 from django.template import loader
 from django.http import HttpResponse, JsonResponse
 
@@ -94,11 +95,12 @@ def new_event_api(request):
         type=data['type'].upper(),
         round=the_round
     )
-    if event.save():
+    try:
+        event.save()
         return JsonResponse({
             "success": True
         })
-    else:
+    except IntegrityError as e:
         return JsonResponse({
             "success": False,
             "reason": "Invalid event type"
@@ -131,12 +133,21 @@ def event_stream(request):
         },
         "rounds": []
     }
-    for round in rounds:
+    # Get round time, status data
+    (
+        round_time,
+        round_status
+    ) = time_remaining(
+        rounds,
+        current_duel_events
+    )
+    for a_round in rounds:
         event_data["rounds"].append(
             {
-                "round_id": round.id,
-                "round_number": round.round_number,
-                "status": round.status
+                "round_id": a_round.id,
+                "round_number": a_round.round_number,
+                "status": round_status[a_round.round_number - 1],
+                "time": round_time[a_round.round_number - 1]
             }
         )
     # TODO add latest event
@@ -148,7 +159,8 @@ def event_stream(request):
 # Current round should be RUNNING or PAUSED
 def current_round(rounds):
     for a_round in rounds:
-        if a_round.status == "RUNNING" or a_round.status == "PAUSED":
+        status = determine_round_status(a_round)
+        if status == "RUNNING" or status == "PAUSED":
             return a_round
 
 
@@ -156,7 +168,7 @@ def next_round(rounds):
     # Get round numbers of all complete rounds
     rounds_complete = []
     for a_round in rounds:
-        if a_round.status == "FINISHED":
+        if determine_round_status(a_round) == "FINISHED":
             rounds_complete.append(
                 a_round.round_number
             )
@@ -170,3 +182,185 @@ def next_round(rounds):
     for a_round in rounds:
         if a_round.round_number == next_round_number:
             return a_round
+
+
+# Helper functions to create event stream data
+
+# Calculate time and round status
+def time_remaining(rounds, events):
+    milliseconds_remaining = [0, 0, 0]
+    status = ["", "", ""]
+    # For each round
+    for a_round in rounds:
+        # Get the events for that round
+        applicable_events = [x for x in events if x.round == a_round]
+        # If the event has not started, 30000 ms
+        # If the event has finished, 0 ms
+        has_started = False
+        has_finished = False
+        for event in applicable_events:
+            if event.type == "START-ROUND":
+                has_started = True
+            if event.type == "FINISH-ROUND":
+                has_finished = True
+                break
+        if has_finished:
+            # Record 0 and go to next round calculation
+            milliseconds_remaining[a_round.round_number - 1] = 0
+            status[a_round.round_number - 1] = "FINISHED"
+            continue
+        if not has_started:
+            # Record 30000 and go to next round calculation
+            milliseconds_remaining[a_round.round_number - 1] = 30000
+            status[a_round.round_number - 1] = "READY"
+            continue
+        # Otherwise, need to do the calculation
+        # Only keep time related events
+        applicable_events = [
+            x for x in applicable_events
+            if (
+                (x.type == "START-ROUND")
+                or (x.type == "PAUSE-ROUND")
+                or (x.type == "CONTINUE-ROUND")
+                or (x.type == "FINISH-ROUND")
+            )
+        ]
+        # The final one will set the status
+        if applicable_events[-1].type == "PAUSE-ROUND":
+            status[a_round.round_number - 1] = "PAUSED"
+        else:
+            status[a_round.round_number - 1] = "RUNNING"
+        # Get the time for the round
+        milliseconds_remaining[a_round.round_number - 1] = calculate_total_time(applicable_events)
+    return (
+        milliseconds_remaining,
+        status
+    )
+
+
+# Given a set of time events
+# calculate total time
+# that a duel has been active
+def calculate_total_time(events):
+    total_time = 0
+    while len(events) > 0:
+        (
+            interval,
+            events
+        ) = next_time_interval(events)
+        if interval is None:
+            return total_time
+        # The pause/finish is left
+        # in the array by the
+        # next_time_interval
+        # function so pop it
+        if len(events) > 0:
+            events.pop(0)
+        total_time += interval
+    return total_time
+
+
+# Get the length in ms
+# of the next active time period
+# in this set of events
+# Return is a tuple (time interval, remaining events)
+def next_time_interval(events):
+    # If there is no start or resume
+    # event then no more active time intervals
+    if not has_start_or_resume(events):
+        return None
+    # Get index of next start/resume
+    start_index = index_of_next_start_or_resume(events)
+    # Get index of subsequent stop, if any
+    stop_index = index_of_subsequent_pause_or_finish(
+        events,
+        start_index
+    )
+    # If stop index is None
+    # active period is still ongoing
+    if stop_index is None:
+        return (
+            difference_in_ms(
+                datetime.datetime.now(),
+                events[start_index].time
+            ),
+            []
+        )
+    else:
+        return (
+            difference_in_ms(
+                events[stop_index].time,
+                events[start_index].time
+            ),
+            events[stop_index:]
+        )
+
+    
+# Is there a start/resume in events?
+def has_start_or_resume(events):
+    return len([x for x in events if x.type == "START-ROUND" or x.type == "CONTINUE-ROUND"]) > 0
+
+
+# Find next start or resume event
+def index_of_next_start_or_resume(events):
+    for i in range(len(events)):
+        if events[i].type == "START-ROUND" or events[i].type == "CONTINUE-ROUND":
+            return i
+    return None
+
+
+# Find next pause or finish event
+# after a certain point
+# (start or resume index)
+def index_of_subsequent_pause_or_finish(events, start_index):
+    for i in range(start_index, len(events)):
+        if events[i].type == "PAUSE-ROUND" or events[i].type == "FINISH-ROUND":
+            return i
+    return None
+
+
+# The difference in ms between two datetimes
+def difference_in_ms(later, earlier):
+    later = make_naive(later)
+    earlier = make_naive(earlier)
+    timedelta = later - earlier
+    return timedelta.total_seconds() * 1000
+
+
+# Recast a datetime object to make it naive
+def make_naive(dt):
+    return datetime.datetime(
+        dt.year,
+        dt.month,
+        dt.day,
+        dt.hour,
+        dt.minute,
+        dt.second,
+        dt.microsecond
+    )
+
+
+# Determine a round's status
+# based on events
+def determine_round_status(a_round):
+    events = FightEvent.objects.filter(
+        round__exact=a_round
+    )
+    # Only get status changing events
+    applicable_events = [
+        x for x in events
+        if (
+                (x.type == "START-ROUND")
+                or (x.type == "PAUSE-ROUND")
+                or (x.type == "CONTINUE-ROUND")
+                or (x.type == "FINISH-ROUND")
+        )
+    ]
+    if len(applicable_events) == 0:
+        return "READY"
+    elif applicable_events[-1].type == "PAUSE-ROUND":
+        return "PAUSED"
+    elif applicable_events[-1].type == "CONTINUE-ROUND" or applicable_events[-1].type == "START-ROUND":
+        return "RUNNING"
+    else:
+        return "FINISHED"
