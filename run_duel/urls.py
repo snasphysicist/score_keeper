2c6e24dbcf6f968b16a8c952cbfb7a074abc76d6
snasphysicist <ih8coldplay@hotmail.com>
2020-03-03 20:14:27 +0800

    Route for event stream view

--- run_duel/urls.py
+++ run_duel/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('new', views.new_duel, name='new_duel'),
     path('api/v1/new_duel', views.new_duel_api, name='new_duel_api'),
     path('current', views.current_duel, name='current_duel'),
-    path('api/v1/event', views.new_event_api, name='new_event_api')
+    path('api/v1/event', views.new_event_api, name='new_event_api'),
+    path('api/v1/event_stream', views.event_stream, name='event_stream'),
 ]
