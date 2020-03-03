1c264ed73bd99d00bab4fb1bec62a07da0dfabc0
snasphysicist <ih8coldplay@hotmail.com>
2020-03-03 19:50:36 +0800

    Fix typo, missing bracket

--- run_duel/urls.py
+++ run_duel/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('new', views.new_duel, name='new_duel'),
     path('api/v1/new_duel', views.new_duel_api, name='new_duel_api'),
     path('current', views.current_duel, name='current_duel'),
-    path('api/v1/event', views.new_event_api, name='new_event_api'
+    path('api/v1/event', views.new_event_api, name='new_event_api')
 ]
