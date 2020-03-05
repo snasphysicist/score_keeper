88f82fd02f128af847bb9f9af12966748ec2c55f
snasphysicist <ih8coldplay@hotmail.com>
2020-03-05 19:49:51 +0800

    Max length for char field, correct default behaviour for date

--- tournament/models.py
+++ tournament/models.py
@@ -1,20 +1,22 @@
 
-import datetime
-
 from django.db import models
+from django.utils import timezone
 
 
 # Tournament model
 class Tournament(models.Model):
     name = models.CharField(max_length=100)
-    date = models.DateField(default=datetime.date.today())
+    date = models.DateField(default=timezone.now)
 
 
 # Stage of a tournament
 class Stage(models.Model):
     number = models.IntegerField(default=0)
     # ROUND-ROBIN, KNOCKOUT
-    stage_format = models.CharField(default="ROUND-ROBIN")
+    stage_format = models.CharField(
+        default="ROUND-ROBIN",
+        max_length=20
+    )
     tournament = models.ForeignKey(
         'Tournament',
         on_delete=models.CASCADE,
