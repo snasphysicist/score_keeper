8b9b7b889967e2a3b936baa4f4182a8d196d8312
snasphysicist <ih8coldplay@hotmail.com>
2020-03-05 19:49:35 +0800

    Install tournament app

--- score_keeper/settings.py
+++ score_keeper/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'run_duel'
+    'run_duel',
+    'tournament'
 ]
 
 MIDDLEWARE = [
