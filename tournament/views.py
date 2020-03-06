72eb72de6671092674b85a2a9d265347990a47f5
snasphysicist <ih8coldplay@hotmail.com>
2020-03-06 17:04:19 +0800

    View function for return tournament overview api data

--- tournament/views.py
+++ tournament/views.py
@@ -1,6 +1,10 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
+
+from run_duel.models import Duel
+from run_duel.views import calculate_duel_data
+from tournament.models import Group, Stage, Tournament
 
 
 # Tournament overview page
@@ -8,3 +12,89 @@ def tournament_overview(request):
     template = loader.get_template('tournament/overview.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+
+def overview_api(request, **kwargs):
+    # Get tournament from id in url
+    tournament_id = kwargs["id"]
+    tournament = list(
+        Tournament.objects.filter(id__exact=tournament_id)
+    )[0]
+    # Get duels for this tournament
+    # via Stages then Groups
+    # Get stages
+    stages = list(
+        Stage.objects.filter(tournament__exact=tournament)
+    )
+    # Get groups
+    groups = list()
+    for stage in stages:
+        groups += list(
+            Group.objects.filter(stage__exact=stage)
+        )
+    # Get duels
+    duels = list()
+    for group in groups:
+        duels += list(
+            Duel.objects.filter(group__exact=group)
+        )
+    # Get data for all duels
+    # and calculate overall scores
+    duels_processed = []
+    for duel in duels:
+        duel_data_raw = calculate_duel_data(duel)
+        duel_data_processed = {}
+        total_score = {
+            "opponent1": 0,
+            "opponent2": 0
+        }
+        for a_round in duel_data_raw["rounds"]:
+            total_score["opponent1"] += a_round["score"]["opponent1"]
+            total_score["opponent2"] += a_round["score"]["opponent2"]
+        duel_data_processed["opponent1"] = duel_data_raw["duel"]["opponent1"]
+        duel_data_processed["opponent2"] = duel_data_raw["duel"]["opponent2"]
+        duel_data_processed["score1"] = total_score["opponent1"]
+        duel_data_processed["score2"] = total_score["opponent2"]
+        duels_processed.append(duel_data_processed)
+    # Have the duels
+    # Work out the participants
+    participants = []
+    participant_names = set(
+        [x["opponent1"] for x in duels_processed]
+        + [x["opponent2"] for x in duels_processed]
+    )
+    for name in participant_names:
+        participant = {
+            "name": name,
+            "wins": 0,
+            "draws": 0,
+            "losses": 0,
+            "total": 0
+        }
+        participant_duels = [
+            x for x in duels_processed
+            if x["opponent1"] == name
+            or x["opponent2"] == name
+        ]
+        for duel in participant_duels:
+            if duel["opponent1"] == name:
+                if duel["score1"] > duel["score2"]:
+                    participant["wins"] += 1
+                elif duel["score1"] < duel["score2"]:
+                    participant["losses"] += 1
+                else:
+                    participant["draws"] += 1
+            else:
+                if duel["score1"] < duel["score2"]:
+                    participant["wins"] += 1
+                elif duel["score1"] > duel["score2"]:
+                    participant["losses"] += 1
+                else:
+                    participant["draws"] += 1
+        participant["total"] = participant["wins"] - participant["losses"]
+        participants.append(participant)
+    return JsonResponse({
+        "duels": duels_processed,
+        "participants": participants,
+        "success": True
+    })
