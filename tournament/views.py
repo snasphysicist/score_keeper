88f4626d3b96931ec7e2812012f8e264370900d2
snasphysicist <ih8coldplay@hotmail.com>
2020-03-06 11:23:17 +0800

    View and url for tournament overview page

--- tournament/views.py
+++ tournament/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+
+# Tournament overview page
+def tournament_overview(request):
+    template = loader.get_template('tournament/overview.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
