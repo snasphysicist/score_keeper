f33417a55d8eaaa162bd9142943f0a2325a96ad9
snasphysicist <ih8coldplay@hotmail.com>
2020-03-06 11:25:43 +0800

    Include tournament app urls in project

--- score_keeper/urls.py
+++ score_keeper/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('run_duel/', include('run_duel.urls'))
+    path('run_duel/', include('run_duel.urls')),
+    path('tournament/', include('tournament.urls'))
 ]
