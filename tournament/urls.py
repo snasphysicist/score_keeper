3bca95e2c8b470bbced88172d51ce3918ab372fa
snasphysicist <ih8coldplay@hotmail.com>
2020-03-09 12:33:19 +0800

    Route to api for duel setup information

--- tournament/urls.py
+++ tournament/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('overview', views.tournament_overview, name='tournament_overview'),
     path('api/v1/<int:id>/overview', views.overview_api, name="overview_api"),
-    path('setup_duels', views.setup_duels, name="setup_duels")
+    path('setup_duels', views.setup_duels, name="setup_duels"),
+    path('api/v1/setup_duels', views.setup_duels_groups_participants_api, name="set_duels_api"),
 ]
