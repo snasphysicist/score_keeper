3c039a7b8d29c29777563d399bb279029209fa2f
snasphysicist <ih8coldplay@hotmail.com>
2020-03-10 12:35:38 +0800

    Route for stages/groups per tournament api

--- tournament/urls.py
+++ tournament/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('api/v1/<int:id>/overview', views.overview_api, name="overview_api"),
     path('setup_duels', views.setup_duels, name="setup_duels"),
     path('api/v1/setup_duels', views.setup_duels_groups_participants_api, name="set_duels_api"),
-    path('api/v1/confirm_duels', views.generate_duels_api, name="generate_duels_api")
+    path('api/v1/confirm_duels', views.generate_duels_api, name="generate_duels_api"),
+    path('api/v1/<int:id>/stagesgroups', views.stages_groups_api, name="stagesgroups_api")
 ]
