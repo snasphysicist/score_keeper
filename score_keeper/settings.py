be6d695e1f5a17551471fd25d1102cf0736af618
snasphysicist <ih8coldplay@hotmail.com>
2020-03-22 12:30:47 +0800

    Take secret key from environment

--- score_keeper/settings.py
+++ score_keeper/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=noqiab!ex(0hrq+)6$@h_mxey*leg=+gf^ekoc@&99iqu5=&x'
+SECRET_KEY = os.environ("SCORE_KEEPER_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
