9cd11d49c9c296b6be743b0b121d8c3cc86fb4df
snasphysicist <ih8coldplay@hotmail.com>
2020-04-16 22:00:18 +0800

    Explicit inclusion of AppConfig to start websocket on server startup

--- score_keeper/settings.py
+++ score_keeper/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'run_duel',
-    'tournament'
+    'score_keeper.apps.WebsocketAppConfig',
+    'tournament',
 ]
 
 MIDDLEWARE = [
