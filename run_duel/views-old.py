a50de9f2b519b5265dbd0d24b9ac4b0798a16d73
snasphysicist <ih8coldplay@hotmail.com>
2020-05-02 15:21:00 +0800

    Move new duel api handler to separate file

--- run_duel/views-old.py
+++ run_duel/views-old.py
@@ -12,44 +12,6 @@ from run_duel.models import Duel, FightEvent, Round
 CURRENT_TOURNAMENT = 3
 
 
-# Move on to a different duel
-def new_duel_api(request):
-    if not can_start_duels(request):
-        return JsonResponse(
-            {
-                "success": False,
-                "reason": "You do not have permission to perform this operation"
-            }
-        )
-    data = json.loads(
-        request.body.decode('utf-8')
-    )
-    # Basically, just make the selected
-    # duel the current one
-    duel = Duel.by_id(
-        None,
-        data["id"]
-    )
-    if duel is None:
-        return JsonResponse(
-            {
-                "success": False,
-                "reason": "The selected duel does not appear to exist"
-            }
-        )
-    # For any other duel marked
-    # as current, make it not so
-    old_duels = Duel.marked_as_current(None)
-    for old_duel in old_duels:
-        old_duel.current = False
-        old_duel.save()
-    duel.current = True
-    duel.save()
-    return JsonResponse({
-        "success": True
-    })
-
-
 # Render current duel page
 def current_duel(request):
     template = loader.get_template('run_duel/current.html')
--- run_duel/views/__init__.py
+++ run_duel/views/__init__.py
@@ -1,5 +1,6 @@
 
 from .get_all_duels_api import handle as get_all_duels_api
 from .new_duel import handle as new_duel
+from .new_duel_api import handle as new_duel_api
 from .new_event_api import handle as new_event_api
 from .pending_duel_api import handle as pending_duel_api
