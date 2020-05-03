9ed7c4d6b9f5327349bbe0968e207aaae15bd1da
snasphysicist <ih8coldplay@hotmail.com>
2020-05-03 15:05:20 +0800

    Move stages/groups api to separate file

--- tournament/views-old.py
+++ tournament/views-old.py
@@ -9,30 +9,3 @@ from django.template import loader
 from run_duel.models import Duel, Round
 from tournament.models import Group, Participant, Tournament
 
-
-def stages_groups_api(request, **kwargs):
-    # Get tournament from id in url
-    tournament_id = kwargs["id"]
-    tournament = Tournament.by_id(
-        None,
-        tournament_id
-    )
-    # Assemble data into format front end expects
-    stagesgroups = {"stages": []}
-    # Get stages and groups for stages
-    stages = tournament.all_stages()
-    for stage in stages:
-        current_stage = {
-            "stage": stage.dictionary(),
-            "group": []
-        }
-        groups = stage.all_groups()
-        for group in groups:
-            current_stage["groups"].append(
-                group.dictionary()
-            )
-        stagesgroups["stages"].append(
-            current_stage
-        )
-    stagesgroups["success"] = True
-    return JsonResponse(stagesgroups)
--- tournament/views/__init__.py
+++ tournament/views/__init__.py
@@ -4,3 +4,4 @@ from .overview import handle as overview
 from .overview_api import handle as overview_api
 from .setup_duels import handle as setup_duels
 from .setup_duels_groups_participants_api import handle as setup_duels_groups_participants_api
+from .stages_groups_api import handle as stages_groups_api
